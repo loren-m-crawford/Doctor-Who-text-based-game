d4597c6e62cd31c9c6f4b71a6e1880625228fa4c
Loren <lmcrawf@gmail.com>
2015-12-15 18:39:59 -0500

    First commit. Have initial classes, including world, room, and player, as well as readme file

---
+++ game.rb
@@ -0,0 +1,65 @@
+class World
+	floors = 3
+	rooms_per_floor_width = 3
+	rooms_per_floor_height = 3
+
+	def initialize
+    @castle = []
+    floors.times do 
+    	@castle << Array.new(rooms_per_floor_height, Array.new(rooms_per_floor_width))
+    end
+  end
+
+  def move_player_forward(player)
+    player.y_coord -= 1 if player.y_coord > 0
+  end
+
+  def move_player_backward(player)
+    player.y_coord += 1 if player.y_coord < floor_height - 1
+  end
+
+  def move_player_right(player)
+    player.x_coord += 1 if player.x_coord < floor_width - 1
+  end
+
+  def move_player_left(player)
+    player.x_coord -= 1 if player.x_coord > 0
+  end
+
+  def move_player_up_floor
+  	player.z_coord += 1 if player.z_coord < floors - 1
+  end
+
+  def move_player_down_floor
+  	player.z_coord -= 1 if player.z_coord > 0
+  end
+
+  def get_room_of(player)
+    @rooms[player.z_coord][player.x_coord][player.y_coord] ||= Room.new
+  end
+
+end
+
+############################################
+
+class Room
+	attr_accessor :size, :content
+
+	def initialize
+    @locked = get_lock_status
+  end
+
+  def get_lock_status
+  	
+  end
+
+end 
+
+############################################
+
+class Player
+
+	def initialize
+    @z_coord, @x_coord, @y_coord = 1, 1, 0
+  end
+end
---
+++ readme.md
@@ -0,0 +1,22 @@
+ #
+
+         /-\
+    _____|#|_____       
+   |_____________|
+  |_______________|
+|||_POLICE_##_BOX_|||
+ | |¯|¯|¯|||¯|¯|¯| | 
+ | |-|-|-|||-|-|-| |
+ | |_|_|_|||_|_|_| |
+ | ||~~~| | |¯¯¯|| |
+ | ||~~~|!|!| O || |
+ | ||~~~| |.|___|| |
+ | ||¯¯¯| | |¯¯¯|| |
+ | ||   | | |   || |
+ | ||___| | |___|| |
+ | ||¯¯¯| | |¯¯¯|| |
+ | ||   | | |   || |
+ | ||___| | |___|| |
+|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|
+
+As you come into this world, something else is also born. You begin your life, and it begins a journey towards you. It moves slowly, but it never stops. Whatever path you take, it will follow - never faster, never slower, always coming. You will run, it will walk. You will rest, it will not. One day, you will linger in the same place too long, you will sit too still, you will sleep too deep. And when, too late, you rise to go, you will notice a second shadow next to yours. Your life will then be over.
