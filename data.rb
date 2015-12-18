672f4ca638ca6a4b1f62fb564c2869558b0f3abf
Loren <lmcrawf@gmail.com>
2015-12-17 23:03:23 -0500

    Edited data

--- data.rb
+++ data.rb
@@ -6,21 +6,21 @@ ROOMS = {
 		'answer' =>'1894',
 		'locked' => true
 	},
-	[0,0,1] => {
+	[0,1,0] => {
 		'room number' => 13,
 		'hint' => 'Going backward would be better than going forward.',
 		'question' => "What is River Song's name at birth?",
 		'answer' =>'melody pond',
 		'locked' => true
 	},
-	[0,0,2] => {
+	[0,2,0] => {
 		'room number' => 9,
 		'hint' => 'You are on the wrong side of the castle.',
 		'question' => "In the episode 'Kill the Moon', what does the moon turn out to be?",
 		'answer' =>'egg',
 		'locked' => true
 	},
-	[0,1,0] => {
+	[0,0,1] => {
 		'room number' => 15,
 		'hint' => 'Go left to find what you seek.',
 		'question' => 'What is the name of the scientist who created the Daleks?',
@@ -34,21 +34,21 @@ ROOMS = {
 		'answer' =>'hi',
 		'locked' => true
 	},
-	[0,1,2] => {
+	[0,2,1] => {
 		'room number' => 16,
 		'hint' => 'You are on the wrong side of the castle.',
 		'question' => "What is the first name of the Doctor's companion who is an actual medical doctor?",
 		'answer' =>'martha',
 		'locked' => true
 	},
-	[0,2,0] => {
+	[0,0,2] => {
 		'room number' => 12,
 		'hint' => 'You have reached room 12. Are you ready to find out who put you here?',
-		'question' => '',
-		'answer' =>'',
+		'question' => 'You have reached room 12. Are you ready to find out who put you here?',
+		'answer' =>'yes',
 		'locked' => true
 	},
-	[0,2,1] => {
+	[0,1,2] => {
 		'room number' => 17,
 		'hint' => 'Go left to find what you seek!',
 		'question' => "What is the name of Rose Tyler's persona?",
@@ -69,21 +69,21 @@ ROOMS = {
 		'answer' =>'',
 		'locked' => false
 	},
-	[1,0,1] => {
+	[1,1,0] => {
 		'room number' => 8,
 		'hint' => "You're going the wrong way.",
 		'question' => 'What planet do the Cybermen originate from?',
 		'answer' =>'mondas',
 		'locked' => true
 	},
-	[1,0,2] => {
+	[1,2,0] => {
 		'room number' => 2,
 		'hint' => 'You are not on the right floor',
 		'question' => "Clara Oswald is better known in 'Asylum of the Daleks' as what?",
 		'answer' =>'souffle girl',
 		'locked' => true
 	},
-	[1,1,0] => {
+	[1,0,1] => {
 		'room number' => 0,
 		'hint' => '',
 		'question' => 'THIS IS THE QUESTION',
@@ -97,21 +97,21 @@ ROOMS = {
 		'answer' =>'',
 		'locked' => false
 	},
-	[1,1,2] => {
+	[1,2,1] => {
 		'room number' => 'hall',
 		'hint' => 'You have found the hall. There are no hints here.',
 		'question' => '',
 		'answer' =>'',
 		'locked' => false
 	},
-	[1,2,0] => {
+	[1,0,2] => {
 		'room number' => 7,
 		'hint' => 'The rooms are not in order',
 		'question' => 'What do the Daleks call the Doctor on their planet?',
 		'answer' =>'the deadly traveller',
 		'locked' => true
 	},
-	[1,2,1] => {
+	[1,1,2] => {
 		'room number' => 1,
 		'hint' => 'There are 18 rooms in the castle, a hall, a courtyard, and a kitchen. The courtyard and hall both take up two floors.',
 		'question' => "What is the name of Amy and Rory's daughter?",
@@ -132,21 +132,21 @@ ROOMS = {
 		'answer' =>'',
 		'locked' => false
 	},
-	[2,0,1] => {
+	[2,1,0] => {
 		'room number' => 3,
 		'hint' => "You're getting colder.",
 		'question' => 'What race has shape-shifting abilities?',
 		'answer' =>'zygons',
 		'locked' => true
 	},
-	[2,0,2] => {
+	[2,2,0] => {
 		'room number' => 6,
 		'hint' => 'If you move down a floor now, you will be in room 2, if you go down further you will be in room 9.',
 		'question' => 'What corporation enslaved the Oods?',
 		'answer' =>'ood operations',
 		'locked' => true
 	},
-	[2,1,0] => {
+	[2,0,1] => {
 		'room number' => 11,
 		'hint' => 'Go forward and what you need to do will be revealed.',
 		'question' => 'What creatures are immediately forgotten after you stop looking at them?',
@@ -160,21 +160,21 @@ ROOMS = {
 		'answer' =>'',
 		'locked' => true
 	},
-	[2,1,2] => {
+	[2,2,1] => {
 		'room number' => 'hall',
 		'hint' => 'You have found the hall. There are no hints here.',
 		'question' => '',
 		'answer' =>'',
 		'locked' => true
 	},
-	[2,2,0] => {
+	[2,0,2] => {
 		'room number' => 5,
 		'hint' => 'You are on top of what you seek.',
 		'question' => 'What did the tenth Doctor say to avoid being displaced in time by the weeping angles?',
 		'answer' =>"don't blink",
 		'locked' => true
 	},
-	[2,2,1] => {
+	[2,1,2] => {
 		'room number' => 14,
 		'hint' => 'You are above, but you need to go below.',
 		'question' => 'What planet is the Doctor buried on?',
