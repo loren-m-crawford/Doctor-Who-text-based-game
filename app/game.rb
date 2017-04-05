7a682326d68401d62949cf4b25a2ecef86bac5c6
Loren Crawford <lorencrawford@msc0l061222.man.co>
2017-04-05 18:34:43 -0400

    Try metaprogramming for game moves

--- app/game.rb
+++ app/game.rb
@@ -1,41 +1,45 @@
 require_relative 'data'
-require_relative 'world'
 require_relative 'room'
 require_relative 'player'
-#require_relative 'game_intro.txt'
 
 class Game
   MOVES = [
     'forward', 'backward', 'right', 'left', 'up', 'down'
   ]
+
+  attr_accessor :standardized_move
+
   def initialize
-    @world = World.new
-    @player = Player.new
+    @player = Player.new(self)
   end
 
   def get_player_move
-    move = gets.downcase.strip
+    move = $stdin.gets.downcase.strip
     check_move(move)
   end
 
   def check_move(move)
     MOVES.each do |m|
-      move = m if m =~ Regexp.new(move)
-      break
+      @standardized_move = m if m =~ Regexp.new(move)
     end
-    give_move_feedback
+    give_move_feedback if @standardized_move.nil?
   end
 
   def give_move_feedback
       puts "That's not a valid move. Please type in 'b for backward', 'f for forward', 'r for right', 'l for left', 'u for up', and 'd for down.'"
   end
 
-  def room_number
-    ROOMS[[@player.z_coord,@player.x_coord,@player.y_coord]]['room number']
+  def check_room_number
+    @player.room_number != 12
   end
 
-  def check_room_number
-     room_number != 12
+  MOVES.each_with_index do |value, index|
+    define_method(value) do
+      if index.even?
+        1
+      else
+        -1
+      end
+    end
   end
 end
-
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../app/game'
+require_relative '../app/player'
 require 'rspec'
 require 'pry'
 require 'spec_helper'
@@ -9,9 +10,13 @@ describe Game do
     $stdin = StringIO.new("DOwN ")
   end
 
+  after do
+     $stdin = STDIN
+  end
+
   describe "#get_player_move" do
     it "downcases and strips the user input" do
-      expect(@game).to receive(:get_player_move).and_return('down')
+      expect(@game).to receive(:check_move).with('down')
       @game.get_player_move
     end
   end
@@ -20,15 +25,65 @@ describe Game do
     context 'user provides incomplete move' do
       it "returns standardized input" do
         move = "righ"
-        expect(@game).to receive(:check_move).with("righ").and_return('right')
+        expect(@game).to receive(:give_move_feedback).never
         @game.check_move(move)
       end
     end
+
     context 'user provides an incorrect move' do
       it 'gives user feedback on moves' do
         move = "geronimo"
         expect(@game).to receive(:give_move_feedback).once
         @game.check_move(move)
+      end
+    end
+  end
+
+  describe '#forward' do
+    context 'user provides forward input' do
+      it 'returns +1 ' do
+        expect(@game.forward).to eq 1
+      end
+    end
+
+    describe '#backward' do
+      context 'user provides backward input' do
+        it 'returns -1' do
+          expect(@game.backward).to eq -1
+        end
+      end
+    end
+
+    describe '#right' do
+      context 'user provides right input' do
+        it 'returns -1' do
+          expect(@game.right).to eq 1
+        end
+      end
+    end
+
+    describe '#left' do
+      context 'user provides left input' do
+        it 'returns -1' do
+          expect(@game.left).to eq -1
+        end
+      end
+    end
+  end
+
+  describe '#check_room_number' do
+    context 'room number is not 12' do
+      it 'returns true' do
+        expect(@game.check_room_number).to be true
+      end
+    end
+
+    context 'room number is 12' do
+      before do
+        allow_any_instance_of(Player).to receive(:room_number).and_return(12)
+      end
+      it 'return false' do
+        expect(@game.check_room_number).to be false
       end
     end
   end
